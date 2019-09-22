2affd3c770c3ad985104ebaf8c381a0e7c56f4e4
Dyan Galih <dyan.galih@gmail.com>
2019-09-23 01:47:51 +0700

    Add default category

--- src/SmartNotify.php
+++ src/SmartNotify.php
@@ -16,7 +16,7 @@ use GuzzleHttp\Client;
  */
 class SmartNotify
 {
-    public static function push(string $message)
+    public static function push(string $message, string $category = 'SYSTEM_ERROR')
     {
         $url = 'https://push.webappid.com/api/notify';
         $client = new Client(['cookies' => true, 'Referer' => $url, 'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36']);
@@ -26,7 +26,7 @@ class SmartNotify
                     'form_params' => [
                         'token' => env('NOTIFY_TOKEN'),
                         'messages' => $message,
-                        'category' => 'SYSTEM_ERROR'
+                        'category' => $category
                     ]
                 ]
             );
