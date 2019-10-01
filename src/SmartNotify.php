430ee744e2e5de726b595b2d8d09d0b37d2f483f
Dyan Galih <dyan.galih@gmail.com>
2019-10-01 18:20:57 +0700

    Supporting register new client

--- src/SmartNotify.php
+++ src/SmartNotify.php
@@ -22,29 +22,73 @@ class SmartNotify
 
     public const WARNING = 'WARNING';
 
-    public const PUSH = 'PUSH';
+    public const PAYMENT_REPORT = 'PUSH';
+
+    private const ENDPOINT = 'https://push.webappid.com/api/';
+
+    private static function getClient(string $url)
+    {
+        return new Client(
+            ['headers' =>
+                [
+                    'cookies' => true,
+                    'Referer' => $url,
+                    'Accept' => 'application/json',
+                    'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+                ]
+            ]);
+    }
 
     public static function push(string $message,
                                 string $category = self::SYSTEM_ERROR,
                                 string $tokenAccount = '',
                                 string $title = '')
     {
-        $url = 'https://push.webappid.com/api/notify';
-        $client = new Client(['cookies' => true, 'Referer' => $url, 'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36']);
-        try {
-            $client->post($url,
-                [
-                    'form_params' => [
-                        'token' => env('NOTIFY_TOKEN'),
-                        'token_account' => $tokenAccount,
-                        'title' => $title,
-                        'messages' => $message,
-                        'category' => $category
+        if (env('NOTIFY_TOKEN') != null) {
+            $url = self::ENDPOINT . 'notify';
+            try {
+                self::getClient($url)->post($url,
+                    [
+                        'form_params' => [
+                            'token' => env('NOTIFY_TOKEN'),
+                            'token_account' => $tokenAccount,
+                            'title' => $title,
+                            'messages' => $message,
+                            'category' => $category
+                        ]
                     ]
-                ]
-            );
-        } catch (Exception $exception) {
-            //nothing to report
+                );
+            } catch (Exception $exception) {
+                //nothing to report
+            }
+        }
+    }
+
+    public static function register(string $registerCode, string $chatId): ?string
+    {
+        if (env('NOTIFY_TOKEN') != null) {
+            $url = self::ENDPOINT . 'register';
+
+            try {
+                $result = self::getClient($url)->post($url,
+                    [
+                        'form_params' => [
+                            'token' => env('NOTIFY_TOKEN'),
+                            'register_code' => $registerCode,
+                            'chat_id' => $chatId
+                        ]
+                    ]
+                );
+
+                $resultJson = json_decode($result->getBody()->getContents());
+
+                if ($resultJson->code == 201) {
+                    return $resultJson->data;
+                }
+            } catch (Exception $exception) {
+                //nothing to report
+                return null;
+            }
         }
     }
 }
